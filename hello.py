1339f805198b13b457e0724bf47b27586480b03f
SMongkon <jothailand@gmail.com>
2024-03-23 15:38:47 +0700

    modified:   hello.py 	new file:   python-webscrapping-hw02.ipynb

--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
-print('hello world!')
+import requests
+from bs4 import BeautifulSoup as soup
+
+url = 'https://www.bangkokpost.com/life/tech'
+r = requests.get(url)
+page_html = r.text
+
+data = soup(page_html, 'html.parser')
+
+links = data.find_all('div', {'class':'news--list-text'})
+
+for l in links:
+    text = l.h3.text
+    text = text.replace('\n','')
+    text = text.replace('\t','')
+    print(text)
+    print('-----')
